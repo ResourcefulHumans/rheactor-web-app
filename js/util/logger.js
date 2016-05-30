6a7cfd668ccdabc54e431466e080190d260d179e
Markus Tacker <m@cto.hiv>
2016-05-30 18:18:08 +0200

    fix(Logger): do not depend on env

--- js/util/logger.js
+++ js/util/logger.js
@@ -1,11 +1,9 @@
 'use strict'
 
-const appName = process.env.npm_package_name
-
 module.exports = {
-  appInfo: console.log.bind(console, '%c ' + appName + ' ', 'background: #3399ff; color: #fff'),
-  appNotice: console.log.bind(console, '%c ' + appName + ' ', 'background: #CC8F14; color: #fff'),
-  appWarning: console.log.bind(console, '%c ⚠ ' + appName + ' ⚠ ', 'background: #CC4B21; color: #fff'),
+  appInfo: console.log.bind(console, '%c App ', 'background: #3399ff; color: #fff'),
+  appNotice: console.log.bind(console, '%c App ', 'background: #CC8F14; color: #fff'),
+  appWarning: console.log.bind(console, '%c ⚠ App ⚠ ', 'background: #CC4B21; color: #fff'),
   authInfo: console.log.bind(console, '%c Auth ', 'background: #F2B646; color: #fff'),
   authNotice: console.log.bind(console, '%c Auth ', 'background: #CC8F14; color: #fff'),
   authWarning: console.log.bind(console, '%c ⚠ Auth ⚠ ', 'background: #CC4B21; color: #fff'),
