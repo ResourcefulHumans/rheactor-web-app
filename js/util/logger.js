c96e18f296917fdfe03b8f5455a284dc3acde322
Markus Tacker <m@cto.hiv>
2016-06-06 14:01:55 +0200

    fix(IE): no styled console logging on IE

--- js/util/logger.js
+++ js/util/logger.js
@@ -1,13 +1,29 @@
 'use strict'
 
-module.exports = {
-  appInfo: console.log.bind(console, '%c App ', 'background: #3399ff; color: #fff'),
-  appNotice: console.log.bind(console, '%c App ', 'background: #CC8F14; color: #fff'),
-  appWarning: console.log.bind(console, '%c ⚠ App ⚠ ', 'background: #CC4B21; color: #fff'),
-  authInfo: console.log.bind(console, '%c Auth ', 'background: #F2B646; color: #fff'),
-  authNotice: console.log.bind(console, '%c Auth ', 'background: #CC8F14; color: #fff'),
-  authWarning: console.log.bind(console, '%c ⚠ Auth ⚠ ', 'background: #CC4B21; color: #fff'),
-  apiInfo: console.log.bind(console, '%c API ', 'background: #99FF33; color: #ccc'),
-  apiNotice: console.log.bind(console, '%c API ', 'background: #CC8F14; color: #fff'),
-  apiWarning: console.log.bind(console, '%c ⚠ API ⚠ ', 'background: #CC4B21; color: #fff')
+/* global: window */
+
+if (window.navigator.userAgent.indexOf('MSIE ') >= 0) {
+  module.exports = {
+    appInfo: console.log.bind(console, '[App] '),
+    appNotice: console.log.bind(console, '[App!] '),
+    appWarning: console.log.bind(console, '[!!App!!] '),
+    authInfo: console.log.bind(console, '[Auth] '),
+    authNotice: console.log.bind(console, '[Auth!] '),
+    authWarning: console.log.bind(console, '[!!Auth!!] '),
+    apiInfo: console.log.bind(console, '[API] '),
+    apiNotice: console.log.bind(console, '[API!] '),
+    apiWarning: console.log.bind(console, '[!!API!!] ')
+  }
+} else {
+  module.exports = {
+    appInfo: console.log.bind(console, '%c App ', 'background: #3399ff; color: #fff'),
+    appNotice: console.log.bind(console, '%c App ', 'background: #CC8F14; color: #fff'),
+    appWarning: console.log.bind(console, '%c ⚠ App ⚠ ', 'background: #CC4B21; color: #fff'),
+    authInfo: console.log.bind(console, '%c Auth ', 'background: #F2B646; color: #fff'),
+    authNotice: console.log.bind(console, '%c Auth ', 'background: #CC8F14; color: #fff'),
+    authWarning: console.log.bind(console, '%c ⚠ Auth ⚠ ', 'background: #CC4B21; color: #fff'),
+    apiInfo: console.log.bind(console, '%c API ', 'background: #99FF33; color: #ccc'),
+    apiNotice: console.log.bind(console, '%c API ', 'background: #CC8F14; color: #fff'),
+    apiWarning: console.log.bind(console, '%c ⚠ API ⚠ ', 'background: #CC4B21; color: #fff')
+  }
 }
