dc72a11a79cc59c8d5a6fe7d7e58d81be8203bd6
Markus Tacker <m@cto.hiv>
2016-07-01 00:48:19 +0200

    test(List): fixed tests

--- test/model/list.spec.js
+++ test/model/list.spec.js
@@ -4,6 +4,7 @@
 
 let List = require('../../js/model/list')
 let User = require('../../js/model/user')
+let Relation = require('../../js/model/relation')
 let expect = require('chai').expect
 
 describe('List', function () {
@@ -14,25 +15,25 @@ describe('List', function () {
       done()
     })
     it('should be true if list has more pages', (done) => {
-      let l = new List(User.$context, [new User()], 11, 0, 10, [])
+      let l = new List(User.$context, [new User()], 11, 0, 10, [new Relation({rel: 'next'})])
       expect(l.hasNext).to.equal(true)
       done()
     })
     it('should be true if list has more pages and list is not the first page', (done) => {
       // context, items, total, offset, itemsPerPage, links
-      let l = new List(User.$context, [new User()], 300, 10, 10, [])
+      let l = new List(User.$context, [new User()], 300, 10, 10, [new Relation({rel: 'prev'}), new Relation({rel: 'next'})])
       expect(l.hasNext).to.equal(true)
       done()
     })
   })
   describe('.hasPrev', function () {
     it('should be false if list is on first page', (done) => {
-      let l = new List(User.$context, [new User()], 1, 0, 10, [])
+      let l = new List(User.$context, [new User()], 1, 0, 10, [new Relation({rel: 'next'})])
       expect(l.hasPrev).to.equal(false)
       done()
     })
     it('should be true if list is not on first page', (done) => {
-      let l = new List(User.$context, [new User()], 11, 10, 10, [])
+      let l = new List(User.$context, [new User()], 11, 10, 10, [new Relation({rel: 'prev'})])
       expect(l.hasPrev).to.equal(true)
       done()
     })
