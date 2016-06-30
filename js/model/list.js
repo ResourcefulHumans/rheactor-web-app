3f0a626b0a9326cabfbcce07922ca41b113618f7
Markus Tacker <m@cto.hiv>
2016-07-01 00:39:44 +0200

    fix(Pagination): hasNext / hasPrev on List model now checks the actual links

--- js/model/list.js
+++ js/model/list.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const _filter = require('lodash/filter')
+
 /**
  * @param {String} context of items
  * @param {Array} items
@@ -19,8 +21,8 @@ function List (context, items, total, offset, itemsPerPage, links) {
   this.$links = links || []
   this.from = offset + 1
   this.to = this.from + items.length - 1
-  this.hasNext = this.total > this.to
-  this.hasPrev = offset > 0
+  this.hasNext = _filter(this.$links, link => link.rel === 'next').length > 0
+  this.hasPrev = _filter(this.$links, link => link.rel === 'prev').length > 0
 }
 
 List.$context = 'https://github.com/RHeactor/nucleus/wiki/JsonLD#List'
