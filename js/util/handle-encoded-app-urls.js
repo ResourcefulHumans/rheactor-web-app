8e2880935d69d010cccd8a8389192393b116fe8e
Markus Tacker <m@coderbyheart.com>
2016-08-04 17:15:28 +0200

    feat(Router): add fix for encoded URLs.

---
+++ js/util/handle-encoded-app-urls.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/*global window */
+
+/**
+ * Fix URLs where the ! has been encoded as %21, happens usually in links from emails
+ */
+if (window.location.hash.substr(1, 3) === '%21') {
+  window.location.hash = '!' + window.location.hash.substr(4)
+}
