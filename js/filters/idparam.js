13f7cc4611d894765043c68d4d08847fbd69ed66
Markus Tacker <markus@resourceful-humans.com>
2016-11-24 12:36:13 +0100

    chore(CS): remove unnecessary escape

--- js/filters/idparam.js
+++ js/filters/idparam.js
@@ -8,6 +8,6 @@ module.exports = ($location, IDService) => {
   return (model) => {
     if (!model || !model.$id) return
     // Remove to local hostname from the url (if present) to save characters
-    return IDService.encode(model.$id.replace($location.absUrl().match(/^(https?:\/\/[^\/]+)/)[1], ''))
+    return IDService.encode(model.$id.replace($location.absUrl().match(/^(https?:\/\/[^/]+)/)[1], ''))
   }
 }
